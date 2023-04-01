2dadf7ca7605700099a8aaf38db050c4760e0013
Alex <alex192003@bk.ru>
2023-04-01 18:47:05 +0500

    Add components to l12

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Profile(){
+   return <div className='content'>
+      <div>
+         <img src="https://vk-oblozhki.ru/photos/big/shestiugol-niki-rendering-svet-forma-339-3732.jpg" alt="" />
+      </div>
+      <div>
+         <img src="https://i.pinimg.com/736x/45/ef/d3/45efd3bcc430b83395f629a05a489ec5.jpg" alt="" />
+      </div>
+
+      <div>
+         My posts
+         <div>
+            New posts
+         </div>
+         <div>
+            post 1
+         </div>
+         <div>
+            post 2
+         </div>
+      </div>
+
+   </div>
+}
+
+export default Profile;

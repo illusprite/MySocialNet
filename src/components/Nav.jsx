319e85c7cec8fcae37c15946e088deacb07ecc61
Alex <alex192003@bk.ru>
2023-04-03 10:18:53 +0500

    l14 revision

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 import s from './Nav.module.css';
 function Nav(){
    return <nav className={s.nav}>
-      <div className={s.item}>
-         <a href="#">Profile</a>
-      </div>
-      <div className={s.item}>
-         <a href="#">News</a>
-      </div>
-      <div className={s.item}>
-         <a href="#">Messenger</a>
-      </div>
-      <div className={s.item}>
-         <a href="#">Friends</a>
-      </div>
-      <div className={s.item}>
-         <a href="#">Music</a>
-      </div>
-      <div className={s.item}>
-         <a href="#">Settings</a>
+      <div className={s.nav__wrapper}>
+         <div className={s.item}>
+            <a href="#">Profile</a>
+         </div>
+         <div className={s.item}>
+            <a href="#">News</a>
+         </div>
+         <div className={s.item}>
+            <a href="#">Messenger</a>
+         </div>
+         <div className={s.item}>
+            <a href="#">Friends</a>
+         </div>
+         <div className={s.item}>
+            <a href="#">Music</a>
+         </div>
+         <div className={s.item}>
+            <a href="#">Settings</a>
+         </div>
       </div>
    </nav>
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import s from './Profile.module.css';
 function Profile(){
    return <div className={s.content}>
-      <div>
+      <div className={s.profile__fon}>
          <img src="https://vk-oblozhki.ru/photos/big/shestiugol-niki-rendering-svet-forma-339-3732.jpg" alt="" />
       </div>
-      <div>
+      <div className={s.profile__logo}>
          <img src="https://i.pinimg.com/736x/45/ef/d3/45efd3bcc430b83395f629a05a489ec5.jpg" alt="" />
       </div>
-
+      <div className={s.profile__info}>
+         <div className={s.name}>
+            Cheshegorov Alexey
+         </div>
+         <div className={s.status}>
+            Prrrr Wake Up!
+         </div>
+         <div className={s.city}>
+            Ekaterinburg
+         </div>
+         <div className={s.age}>
+            Nineteen
+         </div>
+      </div>
       <div className={s.posts}>
          My posts
          <div className={s.item}>

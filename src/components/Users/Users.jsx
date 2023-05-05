fa7e970b297d42908440c48cb65632d620b404e6
Alex <alex192003@bk.ru>
2023-05-06 00:38:09 +0500

    l50-51 Rest Api, Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,31 +1,20 @@
+import axios from 'axios';
 import s from './Users.module.css';
-
+import userPhoto from '../../assets/images/f330830a9bcecaa040636c4ab357277d.jpg';
 let Users = (props) => {
-   props.setUsers([
-      {
-         id:1,   
-         photoUrl:'https://static5.tgstat.ru/channels/_0/37/3728e8578f53e2f9fa678cb4d0a4a955.jpg', 
-         followed: true, fullName:'Alexey', status:"I'am a boss", location:{city: "Ekaterinburg", country: "Russia"}},
-      {
-         id:2,   
-         photoUrl:'https://i.pinimg.com/236x/ed/3d/5f/ed3d5fa3cf189eb02382f3607fbcf5c8.jpg', 
-         followed: false, fullName:"Anton", status:"Get stars", location:{city: "Ekaterinburg", country: "Russia"}},
-      {
-         id:3,   
-         photoUrl:'https://i.pinimg.com/236x/6f/32/86/6f328665221792e21c97bf2efa94ef92.jpg', 
-         followed: true, fullName:'Pavel', status:"I working", location:{city: "Ekaterinburg", country: "Russia"}},
-      {
-         id:4,   
-         photoUrl:'https://www.kino-teatr.ru/movie/kadr/153511/pv_1283010.jpg', 
-         followed: true, fullName:'Andrew', status:"I'am a really boss", location:{city: "Ekaterinburg", country: "Russia"}
-      },
-   ]);
+   if(props.users.length === 0){
+      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+         props.setUsers(response.data.items);
+      });
+
+      
+   }
    return <div className={s.item}>
       {
       props.users.map(u => <div key={u.id}>
          <span>
             <div>
-               <img className={s.userPhoto} src={u.photoUrl} alt="" />
+               <img className={s.userPhoto} src={u.photos.small !=null ? u.photos.small : userPhoto} alt="" />
             </div>
             <div>
                { u.followed 
@@ -36,12 +25,12 @@ let Users = (props) => {
          </span>
          <span>
             <span>
-               <div>{u.fullName}</div>
+               <div>{u.name}</div>
                <div>{u.status}</div>
             </span>
             <span>
-               <div>{u.location.country}</div>
-               <div>{u.location.city}</div>
+               <div>{"u.location.country"}</div>
+               <div>{"u.location.city"}</div>
             </span>
          </span>
       </div>)
--- src/redux/usersPage-reducer.js
+++ src/redux/usersPage-reducer.js
@@ -34,7 +34,7 @@ const usersPageReducer = (state = initialState, action) => {
       case SET_USERS:{
          return {
             ...state,
-            users: [...state.users, action.users]
+            users: [...action.users]
          };
       };
       default:{
